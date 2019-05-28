0ccdee21eafbcaf80b1af9906def0b910ba6ab7d
Puchenkin <apuchenk@merann.ru>
2019-05-28 11:30:21 +0300

    initial

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+const Hello = () => (
+  <h1>React stepper</h1>
+);
+
+ReactDOM.render(
+  <Hello />,
+  document.getElementById("root")
+);
